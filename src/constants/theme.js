7aca7f156c31f47307a092af656d5d61031550fe
Markaa10 <rahulrajdahal@gmail.com>
2021-03-22 16:36:39 +0545

    themejs

---
+++ src/constants/theme.js
@@ -0,0 +1,59 @@
+import { Dimensions } from "react-native";
+const { widht, height } = Dimensions.get("window");
+
+export const COLOR = {
+  primary: "#0984E3",
+
+  text: "#333333",
+  textSeconday: "#727272",
+
+  white: "#ffffff",
+  black: "#000000",
+};
+
+export const SIZES = {
+  h1: 32,
+  h2: 24,
+  h3: 18,
+
+  body1: 16,
+  body2: 14,
+  body3: 12,
+  // app dimensions
+  widht,
+  height,
+};
+
+export const FONTS = {
+  h1: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.h1,
+    lineHeight: 32,
+  },
+  h2: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.h2,
+    lineHeight: 24,
+  },
+  h3: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.h3,
+    lineHeight: 18,
+  },
+
+  body1: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.body1,
+    lineHeight: 16,
+  },
+  body2: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.body2,
+    lineHeight: 14,
+  },
+  body3: {
+    fontFamily: "Gilroy",
+    fontSize: SIZES.body3,
+    lineHeight: 12,
+  },
+};
