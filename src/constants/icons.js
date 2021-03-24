e9c18d9fc2d9fcd8230d26bb9615f52f6b23463a
Markaa10 <rahulrajdahal@gmail.com>
2021-03-24 19:29:16 +0545

    fab

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -3,5 +3,6 @@ import HomeIcon from "../../assets/icons/home.svg";
 import DocumentIcon from "../../assets/icons/document.svg";
 import TodoIcon from "../../assets/icons/todo-icon.svg";
 import GalleryIcon from "../../assets/icons/gallery.svg";
+import Plus from "../../assets/icons/plus.svg";
 
-export { Profile, HomeIcon, DocumentIcon, TodoIcon, GalleryIcon };
+export { Plus, Profile, HomeIcon, DocumentIcon, TodoIcon, GalleryIcon };
--- src/screens/todos.js
+++ src/screens/todos.js
@@ -2,15 +2,40 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Navbar } from "../components";
 import { COLORS } from "../constants";
+import { Plus } from "../constants/icons";
 
 const Todos = () => {
+  function renderFAB() {
+    return (
+      <View
+        style={{
+          backgroundColor: COLORS.primary,
+          width: 72,
+          height: 72,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 28,
+          elevation: 15,
+          shadowRadius: 50,
+          shadowColor: "rgba(9, 132, 227, 0.5)",
+          shadowOffset: 15,
+          position: "absolute",
+          bottom: 24,
+          right: 24,
+        }}
+      >
+        <Plus width={21} height={32} />
+      </View>
+    );
+  }
+
   return (
     <View
       style={{ flex: 1, paddingHorizontal: 24, backgroundColor: COLORS.white }}
     >
       <Navbar lightText="Your" text="Todo List" />
 
-      
+      {renderFAB()}
     </View>
   );
 };
