00b42ef9960bfd06f73c5925d8db1f3c37ba5dde
Markaa10 <rahulrajdahal@gmail.com>
2021-03-25 22:23:42 +0545

    toptabpages

--- src/screens/completed.js
+++ src/screens/completed.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { COLORS } from "../constants";
 
 const Completed = () => {
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: COLORS.white }}>
       <Text>Completed</Text>
     </View>
   );
