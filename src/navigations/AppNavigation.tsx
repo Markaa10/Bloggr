e387927a3d20b12728e822591f08f77789060baf
rahulrajdahal <rahulrajdahal@gmail.com>
2024-08-13 13:33:22 +0545

    initial

---
+++ src/navigations/AppNavigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AppNavigation = () => {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+    </View>
+  );
+};
+
+export default AppNavigation;
+
+const styles = StyleSheet.create({});
